c2aca89548751dd5d0e1fd4fbebbfcb1826200de
chris <uraniumkid30@gmail.com>
2025-01-27 23:33:04 +0100

    day 5

---
+++ DAY0005/Day0005.java
@@ -0,0 +1,15 @@
+package DAY0005;
+
+class Processor <T> {
+    T file;
+    Processor(T file){
+        this.file = file;
+    }
+}
+
+public class Day0005 {
+    static void main(String[] args){
+        Processor<String> pdf_processor = new Processor<String>("java.pdf");
+        System.out.println(pdf_processor.file);
+    }
+}
