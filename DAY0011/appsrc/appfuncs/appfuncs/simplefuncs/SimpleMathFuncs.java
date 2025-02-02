1982a469eff4df9f9b1ee7393e7df5dc8778febd
chris <uraniumkid30@gmail.com>
2025-02-02 22:05:34 +0100

    day 11

---
+++ DAY0011/appsrc/appfuncs/appfuncs/simplefuncs/SimpleMathFuncs.java
@@ -0,0 +1,30 @@
+package DAY0011.appsrc.appfuncs.appfuncs.simplefuncs;
+
+public class SimpleMathFuncs {
+    public static boolean isFactor(int a, int b){
+        return b % a == 0;
+    }
+    public static int lcf(int a, int b){
+        a = Math.abs(a);
+        b = Math.abs(b);
+        int min = a < b ? a : b;
+        for (int i = 2; i <= min/2; i++){
+            if (isFactor(i, a) && isFactor(i, b)){
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    public static int gcf(int a, int b){
+        a = Math.abs(a);
+        b = Math.abs(b);
+        int min = a < b ? a : b;
+        for (int i = min/2; i >= 2; i--){
+            if (isFactor(i, a) && isFactor(i, b)){
+                return i;
+            }
+        }
+        return 1;
+    }
+}
---
+++ DAY0011/appsrc/appstart/appstart/modappdemo/ModAppDemo.java
@@ -0,0 +1,11 @@
+package DAY0011.appsrc.appstart.appstart.modappdemo;
+import appfuncs.simplefuncs.SimpleMathFuncs;
+
+public class ModAppDemo {
+    public static void main(String[] args) {
+        if (SimpleMathFuncs.isFactor(2, 10)){
+            System.out.println("2 is a factor of 10");
+        }
+        System.out.println("Done!");
+    }
+}
