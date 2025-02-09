381522673e342c5f30fabee19fa4279032d11082
chris <uraniumkid30@gmail.com>
2025-02-09 23:27:44 +0100

    day 18

---
+++ DAY0018/Day0018.java
@@ -0,0 +1,12 @@
+package DAY0018;
+final class Person{
+    void printObj(){
+        System.out.println("Person Object");
+    }
+}
+
+class Day0018{
+    public static void main(String[] args) {
+        Person dave = new Person();
+    }
+}
